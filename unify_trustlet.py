5ae6b07fa5d93992470e66cda55972d9f9a364f7
leommxj <leommxj@gmail.com>
2019-12-23 17:47:32 +0800

    convert to python3 ; support 64bit elf

--- unify_trustlet.py
+++ unify_trustlet.py
@@ -1,16 +1,19 @@
 import sys, os, struct
+elf_type = None
+ELF_HEADER_SIZE = lambda: 0x34 if elf_type == 1 else 0x40
+E_PHNUM_OFFSET = lambda: 0x2C if elf_type == 1 else 0x38
+E_TYPE_OFFSET = 0x4
 
-ELF_HEADER_SIZE = 0x34
-E_PHNUM_OFFSET = 0x2C
-PHDR_SIZE = 0x20
-P_FILESZ_OFFSET = 0x10
-P_OFFSET_OFFSET = 0x4
+PHDR_SIZE = lambda :0x20 if elf_type == 1 else 0x38
+P_FILESZ_OFFSET = lambda: 0x10 if elf_type == 1 else 0x20
+P_OFFSET_OFFSET = lambda: 0x4 if elf_type == 1 else 0x8
+INT_LEN = lambda: 0x4 if elf_type == 1 else 0x8
 
 def main():
 
 	#Reading the arguments
 	if len(sys.argv) != 4:
-		print "USAGE: <TRUSTLET_DIR> <TRUSTLET_NAME> <OUTPUT_FILE_PATH>"
+		print("USAGE: <TRUSTLET_DIR> <TRUSTLET_NAME> <OUTPUT_FILE_PATH>")
 		return
 	trustlet_dir = sys.argv[1]
 	trustlet_name = sys.argv[2]
@@ -18,23 +21,35 @@ def main():
 
 	#Reading the ELF header from the ".mdt" file
 	mdt = open(os.path.join(trustlet_dir, "%s.mdt" % trustlet_name), "rb")
-	elf_header = mdt.read(ELF_HEADER_SIZE)
-	phnum = struct.unpack("<H", elf_header[E_PHNUM_OFFSET:E_PHNUM_OFFSET+2])[0]
-	print "[+] Found %d program headers" % phnum
+	
+	_e = mdt.read(0x5)
+	mdt.seek(0)
+	global elf_type
+	elf_type = _e[E_TYPE_OFFSET]
+	if elf_type!=1 and elf_type!=2:
+		exit()
+	elf_header = mdt.read(ELF_HEADER_SIZE())
+
+	print(elf_type)
+	print(E_PHNUM_OFFSET())
+	print(ELF_HEADER_SIZE())
+	phnum = struct.unpack("<H", elf_header[E_PHNUM_OFFSET():E_PHNUM_OFFSET()+2])[0]
+	print("[+] Found %d program headers" % phnum)
 	
 	#Reading each of the program headers and copying the relevant chunk
 	output_file = open(output_file_path, 'wb')
 	for i in range(0, phnum):
 
 		#Reading the PHDR
-		print "[+] Reading PHDR %d" % i
-		phdr = mdt.read(PHDR_SIZE) 	
-		p_filesz = struct.unpack("<I", phdr[P_FILESZ_OFFSET:P_FILESZ_OFFSET+4])[0] 
-		p_offset= struct.unpack("<I", phdr[P_OFFSET_OFFSET:P_OFFSET_OFFSET+4])[0] 
-		print "[+] Size: 0x%08X, Offset: 0x%08X" % (p_filesz, p_offset)
+		print("[+] Reading PHDR %d" % i)
+		phdr = mdt.read(PHDR_SIZE())
+
+		p_filesz = struct.unpack("<I" if INT_LEN()==4 else "<Q", phdr[P_FILESZ_OFFSET():P_FILESZ_OFFSET()+INT_LEN()])[0] 
+		p_offset= struct.unpack("<I" if INT_LEN()==4 else "<Q", phdr[P_OFFSET_OFFSET():P_OFFSET_OFFSET()+INT_LEN()])[0] 
+		print("[+] Size: 0x%08X, Offset: 0x%08X" % (p_filesz, p_offset))
 
 		if p_filesz == 0:
-			print "[+] Empty block, skipping"
+			print("[+] Empty block, skipping")
 			continue #There's no backing block
 
 		#Copying out the data in the block
